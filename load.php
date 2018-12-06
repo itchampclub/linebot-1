4c23e12a8c05822df1579e4305164fe1e5ca18e7
Ittirit Thipphinit <itchampclub@gmail.com>
2018-12-06 09:47:04 +0700

    Update load.php

--- load.php
+++ load.php
@@ -1,7 +1,7 @@
 https://github.com/igrr/esptool-ck/releases/download/0.4.13/esptool-0.4.13-win32.zip
 
 
-https://github.com/igrr/mkspiffs/releases/download/0.2.3/mkspiffs-0.2.3-arduino-esp32-win32.zip
+https://github.com/igrr/mkspiffs/releases/download/0.2.3/mkspiffs-0.2.3-arduino-esp8266-win32.zip
 
 
 
