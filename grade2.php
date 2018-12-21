3e743ad4e9310ec8c907b2591d13efea6069c35f
Ittirit Thipphinit <itchampclub@gmail.com>
2018-12-21 19:38:43 +0700

    Create grade2.php

---
+++ grade2.php
@@ -0,0 +1,2 @@
+
+<form action=http://www.stou.ac.th/Website/QuestGrade/login.aspx method=GET><br><input name="ctl00$ContentPlaceHolder1$TextBox1" type="text" id="ContentPlaceHolder1_TextBox1" style="width:114px;"><br><input type="submit" name="ctl00$ContentPlaceHolder1$Button1" value="ตกลง" id="ContentPlaceHolder1_Button1">
