bf159558976e7d4989b14481b4bd19f91ebefb85
Ittirit Thipphinit <itchampclub@gmail.com>
2018-12-21 19:53:04 +0700

    Update grade2.php

--- grade2.php
+++ grade2.php
@@ -1,5 +1,4 @@
-
-  <form method="post" action="http://www.stou.ac.th/Website/QuestGrade/login.aspx" id="form1">
+<html><head></head><body><form method="post" action="http://www.stou.ac.th/Website/QuestGrade/login.aspx" id="form1">
 <div class="aspNetHidden">
 <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUKLTE5MDgyNDUwOA9kFgJmD2QWAgIDD2QWBAIBDw8WAh4EVGV4dAVf4Liq4Lit4Lia4LiL4LmI4Lit4Lih4Lig4Liy4LiEIDIg4LmB4Lil4Liw4Lig4Liy4LiE4LiB4Liy4Lij4Lio4Li24LiB4Lip4Liy4Lie4Li04LmA4Lio4LipLzI1NjBkZAIDD2QWBAIDD2QWAgIHDw8WAh8ABcIJ4LiC4LmJ4Lit4Lih4Li54Lil4Lih4Li14Lib4Lix4LiN4Lir4LiyU3lzdGVtLkRhdGEuT2xlRGIuT2xlRGJFeGNlcHRpb24gKDB4ODAwNDBFMTQpOiBJbnZhbGlkIGNvbHVtbiBuYW1lICdkYXRldGltbWUnLg0KICAgYXQgU3lzdGVtLkRhdGEuT2xlRGIuT2xlRGJDb21tYW5kLkV4ZWN1dGVDb21tYW5kVGV4dEVycm9ySGFuZGxpbmcoT2xlRGJIUmVzdWx0IGhyKQ0KICAgYXQgU3lzdGVtLkRhdGEuT2xlRGIuT2xlRGJDb21tYW5kLkV4ZWN1dGVDb21tYW5kVGV4dEZvclNpbmdsZVJlc3VsdCh0YWdEQlBBUkFNUyBkYlBhcmFtcywgT2JqZWN0JiBleGVjdXRlUmVzdWx0KQ0KICAgYXQgU3lzdGVtLkRhdGEuT2xlRGIuT2xlRGJDb21tYW5kLkV4ZWN1dGVDb21tYW5kVGV4dChPYmplY3QmIGV4ZWN1dGVSZXN1bHQpDQogICBhdCBTeXN0ZW0uRGF0YS5PbGVEYi5PbGVEYkNvbW1hbmQuRXhlY3V0ZUNvbW1hbmQoQ29tbWFuZEJlaGF2aW9yIGJlaGF2aW9yLCBPYmplY3QmIGV4ZWN1dGVSZXN1bHQpDQogICBhdCBTeXN0ZW0uRGF0YS5PbGVEYi5PbGVEYkNvbW1hbmQuRXhlY3V0ZVJlYWRlckludGVybmFsKENvbW1hbmRCZWhhdmlvciBiZWhhdmlvciwgU3RyaW5nIG1ldGhvZCkNCiAgIGF0IFN5c3RlbS5EYXRhLk9sZURiLk9sZURiQ29tbWFuZC5FeGVjdXRlUmVhZGVyKENvbW1hbmRCZWhhdmlvciBiZWhhdmlvcikNCiAgIGF0IFN5c3RlbS5EYXRhLk9sZURiLk9sZURiQ29tbWFuZC5TeXN0ZW0uRGF0YS5JRGJDb21tYW5kLkV4ZWN1dGVSZWFkZXIoQ29tbWFuZEJlaGF2aW9yIGJlaGF2aW9yKQ0KICAgYXQgU3lzdGVtLkRhdGEuQ29tbW9uLkRiRGF0YUFkYXB0ZXIuRmlsbEludGVybmFsKERhdGFTZXQgZGF0YXNldCwgRGF0YVRhYmxlW10gZGF0YXRhYmxlcywgSW50MzIgc3RhcnRSZWNvcmQsIEludDMyIG1heFJlY29yZHMsIFN0cmluZyBzcmNUYWJsZSwgSURiQ29tbWFuZCBjb21tYW5kLCBDb21tYW5kQmVoYXZpb3IgYmVoYXZpb3IpDQogICBhdCBTeXN0ZW0uRGF0YS5Db21tb24uRGJEYXRhQWRhcHRlci5GaWxsKERhdGFUYWJsZVtdIGRhdGFUYWJsZXMsIEludDMyIHN0YXJ0UmVjb3JkLCBJbnQzMiBtYXhSZWNvcmRzLCBJRGJDb21tYW5kIGNvbW1hbmQsIENvbW1hbmRCZWhhdmlvciBiZWhhdmlvcikNCiAgIGF0IFN5c3RlbS5EYXRhLkNvbW1vbi5EYkRhdGFBZGFwdGVyLkZpbGwoRGF0YVRhYmxlIGRhdGFUYWJsZSkNCiAgIGF0IGxvZ2luLkRhdGUxMSgpZGQCBQ88KwARAwAPFgIeDERhdGFTb3VyY2VJRGVkARAWABYAFgAMFCsAAGQYAQUjY3RsMDAkQ29udGVudFBsYWNlSG9sZGVyMSRHcmlkVmlldzEPZ2QsbxS7rTdBYYospclhuclLOTiBHsgvg7cV3DyFacVeOQ==">
 </div>
@@ -11,34 +10,13 @@
 </div>
     <div>
       <table style="height:100%" border="0" cellpadding="0" cellspacing="0">
-            <tbody><tr>
-                <td class="tdx">
-                    <img src="Pic/Symbol_STOU_C5.gif" class="imx1">
-				</td>
-                <td style="text-align: right; padding-right: 20px;">
-                    <img src="Pic/stou_pic.jpg" class="imx2">
-				</td>
-			</tr>
+            <tbody>
             <tr>
-                <td style=" text-align: center; background-color: #D3EBBC;" colspan="2">
-                   
-                    
-                    <br>
-                    <strong><span class="txhead">ผลการ<span id="txtsemester">สอบซ่อมภาค 2 และภาคการศึกษาพิเศษ/2560</span>
-&nbsp;</span></strong></td>
-                </tr><tr>
-                <td style="padding: 10px; background-color: #003300;" colspan="2" align="center">
+                <td colspan="2" align="center">
         
     <p>
         </p><table>
-            <tbody><tr>
-                <td style="padding: 5px" align="center">
-                
-                    <span class="style2">
-                                <span id="ContentPlaceHolder1_Label2" style="color: #FFFFFF; font-weight: 700; font-size: x-large;"></span>
-					 
-					
-            </span></td></tr>
+            <tbody>
             <tr>
                
                   <td style="padding: 10px; background-color: #009933; color: #003366;" align="center">
@@ -48,24 +26,10 @@
                                 <input name="ctl00$ContentPlaceHolder1$TextBox1" type="text" id="ContentPlaceHolder1_TextBox1" style="width:114px;">
                                 &nbsp;&nbsp;
                                 <input type="submit" name="ctl00$ContentPlaceHolder1$Button1" value="ตกลง" id="ContentPlaceHolder1_Button1">
-                                <strong><span class="style8">
-                                <br>
-                                <br>
-                                Update 20/12/2561 </span></strong>
+                                
                                 <br>
                                 <span id="ContentPlaceHolder1_Label3" style="color: #009933; font-weight: 700; font-size: x-large"></span>
-                                <span id="ContentPlaceHolder1_Label1" style="color: #009933; font-weight: 700">ข้อมูลมีปัญหาSystem.Data.OleDb.OleDbException (0x80040E14): Invalid column name 'datetimme'.
-   at System.Data.OleDb.OleDbCommand.ExecuteCommandTextErrorHandling(OleDbHResult hr)
-   at System.Data.OleDb.OleDbCommand.ExecuteCommandTextForSingleResult(tagDBPARAMS dbParams, Object&amp; executeResult)
-   at System.Data.OleDb.OleDbCommand.ExecuteCommandText(Object&amp; executeResult)
-   at System.Data.OleDb.OleDbCommand.ExecuteCommand(CommandBehavior behavior, Object&amp; executeResult)
-   at System.Data.OleDb.OleDbCommand.ExecuteReaderInternal(CommandBehavior behavior, String method)
-   at System.Data.OleDb.OleDbCommand.ExecuteReader(CommandBehavior behavior)
-   at System.Data.OleDb.OleDbCommand.System.Data.IDbCommand.ExecuteReader(CommandBehavior behavior)
-   at System.Data.Common.DbDataAdapter.FillInternal(DataSet dataset, DataTable[] datatables, Int32 startRecord, Int32 maxRecords, String srcTable, IDbCommand command, CommandBehavior behavior)
-   at System.Data.Common.DbDataAdapter.Fill(DataTable[] dataTables, Int32 startRecord, Int32 maxRecords, IDbCommand command, CommandBehavior behavior)
-   at System.Data.Common.DbDataAdapter.Fill(DataTable dataTable)
-   at login.Date11()</span>
+                                
                                 
                                 <!--<br />-->
                                 
@@ -79,36 +43,10 @@
                 
              
             </tr>
-            <tr>
+            
                
-                  <td style="padding: 10px; background-color: #009933; color: #003366;" align="center"><div class="ct">
-                    <div>
-
-</div></div>
-                        
-                            </td> 
-                
-             
-            </tr>
-               <tr>
-                <td>
-                    <table style="border-style: dashed; border-color: #000000; margin-top:40px;">
-                        <tbody><tr>
-               <td style="background-color: #FFFF00" align="center">
-                  <p style="color :#CC0000;">NA = รอผลการสอบภาคปฏิบัติ</p>
-    
-               "การประกาศผลการสอบครั้งนี้ จะประกาศเฉพาะนักศึกษาที่เข้าสอบเท่านั้น"<br>
-			   <!--
-				   <h3><a href="http://www.stou.ac.th/Offices/ore/adre/regis/reghandbook159.pdf#page=48" target="_blank" title="คลิกดูรายละเอียด">คำชี้แจงการลงทะเบียนสอบซ่อม</a></h3>
-				 -->  
-				   </td>
-
-                      </tr>
-                    </tbody></table>
-                </td>
-            </tr>
         </tbody></table>
-    <p></p>
+    
 <script type="text/javascript">
 	$(window).load( function() {
 		$(document).responsiveTables();
@@ -118,20 +56,8 @@
                     </td>
             </tr>
             
-            <tr>
-                <td style="padding: 5px; background-color: #003300; text-align: center;" colspan="2">
-                    <span class="Btt100">จัดทำโดย </span>
-                    <br class="Btt1">
-                    <span class="Btt1">ฝ่ายวิเคราะห์และพัฒนาระบบ สำนักคอมพิวเตอร์
-มหาวิทยาลัยสุโขทัยธรรมาธิราช
-สงวนลิขสิทธิ์ 
-                    <br>
-                    พ.ศ.2545 ตามพระราชบัญญัติลิขสิทธิ์ พ.ศ.2537 
-                    <br>
-                    Webmaster@stou.ac.th </span>
-
-</td>
-            </tr>
+            
         </tbody></table>
     </div>
     </form>
+</body></html>
