9dad6ba9f46733bdd86f9e1492875d67a93463b1
Ittirit Thipphinit <itchampclub@gmail.com>
2018-12-24 12:49:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,8 +1,5 @@
 <?php
 require_once('./line_class.php');
-require_once('./unirest-php-master/src/Unirest.php');
-include './GoogleTranslate.php';
-
 
 
 $channelAccessToken = 'WsEg0h0hvWL6AH/5vRTp/VoKgHexRMQ+FOgbI9xrJ19q07jk59Z4X9p6laKD7BR6s8F8E3rZ0pvht4n4NOAtNkA726d4quuAYJW/P0rqABDermZI5505WTp5ix0BjLn6WVb67TpH/sIl6Bwv7m+yagdB04t89/1O/w1cDnyilFU='; //sesuaikan 
